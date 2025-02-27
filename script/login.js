d9e63955f61bc4804309585513c50f0d8a5b5f6f
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-02-27 10:04:01 +0600

    Add main.html and make it dynamic

--- script/login.js
+++ script/login.js
@@ -1,7 +1,17 @@
 document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
     const accountNumber = document.getElementById('mobile-number').value;
-    const pin = document.getElementById('pin-number');
-    console.log(accountNumber ,pin);
-
+    const pin = document.getElementById('pin-number').value;
+    const convertedPin = parseInt(pin);
+    if(accountNumber.length === 11){
+        if(convertedPin === 1234){
+            window.location.href = './main.html'
+        }
+        else{
+            alert('incorrect PIN')
+        }
+    }
+    else{
+        alert('Need a valid number');
+    }
 })
